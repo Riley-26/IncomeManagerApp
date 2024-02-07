abd161fdcb275957f49f2215fc0ca7b755c26735
Riley-26 <rileyread1@gmail.com>
2024-02-07 11:55:56 +0000

    7/2/24

--- IncomeManager/settings.py
+++ IncomeManager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)8&lam4h7f_d^2h#i4z+gj+^w!2=47r(fr&$-qzhtd6$e7r0l=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
